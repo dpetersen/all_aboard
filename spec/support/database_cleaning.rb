b6843f46e01ac5cb027b08286a4c5f794dd78bb1
Don Petersen <don@donpetersen.net>
2013-10-09 17:09:33 +0000

    Switch to truncation cleaning strategy across the board.
    Something hokey has started happening when it moves from one JS spec to
    the next.  It gets stuck in the middle of a transaction.  I have used
    the dynamic strategy choosing thing in another app with success, but
    it's failing here and I don't want to deal with it right now.

--- spec/support/database_cleaning.rb
+++ spec/support/database_cleaning.rb
@@ -1,16 +1,7 @@
 require 'database_cleaner'
 
 RSpec.configure do |config|
-  DatabaseCleaner.strategy = :transaction
-
-  config.around(:each) do |example|
-    if example.metadata[:js]
-      example.run
-      DatabaseCleaner.clean_with(:truncation)
-    else
-      DatabaseCleaner.start
-      example.run
-      DatabaseCleaner.clean
-    end
+  config.before(:each) do |example|
+    DatabaseCleaner.clean_with(:truncation)
   end
 end
