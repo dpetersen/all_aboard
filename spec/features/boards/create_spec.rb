e6c625d9ed309aba534208147a1d403ba510f3f1
Don Petersen <don@donpetersen.net>
2013-10-10 16:59:28 +0000

    Don't link to a board that isn't saved yet.
    That wasn't so painful, but I'm worried about that test maybe failing if
    the suite is slower than the server and browser.

--- spec/features/boards/create_spec.rb
+++ spec/features/boards/create_spec.rb
@@ -15,7 +15,7 @@ describe "Board creation", :js do
       visit engine_routes.boards_path
       fill_in "Name", with: "New Board Name"
       click_button "Create"
-      expect(page).to have_content("New Board Name")
+      expect(page).to have_content("Saving New Board Name...")
       expect(page).to have_link("New Board Name")
 
       click_link "New Board Name"
