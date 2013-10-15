f55b85de11135f510288a6729aae0abf02e45eb1
Don Petersen <don@donpetersen.net>
2013-10-15 16:15:51 +0000

    Fix filesystem ordering bug and refactor.
    The main idea was to fix an intermittent issue on CI where templates
    were coming back in a different order.  I'm guessing filesystems on
    different test instances were returning files in different orders
    sometimes.  It'd be a good idea to explicitly sort them anyway, since
    their ordering will be displayed to the end user, too.
    
    Refactored a tiny bit while I was in there.

--- lib/all_aboard/source/perspectives.rb
+++ lib/all_aboard/source/perspectives.rb
@@ -23,19 +23,32 @@ module AllAboard::Source::Perspectives
     def load_templates
       return if @templates_loaded
 
-      template_path = filesystem_path.join("templates").to_s
-      Dir[template_path + "/*"].each do |template_full_path|
-        template_relative_path = template_full_path.split("/").last
-        file_elements = template_relative_path.split(".")
-        basename = file_elements.first
-        width, height = file_elements.second.split("x")
-        perspective = @perspectives.detect { |p| p.filename.to_s == basename }
-
-        markup = File.read(template_full_path)
-        perspective.add_template(width.to_i, height.to_i, markup)
+      templates_path = filesystem_path.join("templates").to_s
+      each_template_for_path(templates_path) do |template_full_path|
+        load_template(template_full_path)
       end
 
       @templates_loaded = true
+    end
+
+    def each_template_for_path(templates_path)
+      Dir[templates_path + "/*"].sort.each do |template_full_path|
+        yield template_full_path
+      end
+    end
+
+    def load_template(template_full_path)
+      # TODO - this variable extraction could go in a class.  The only problem
+      # is that adding filesystem awareness to a bunch of classes will make
+      # testing that more painful.
+      template_relative_path = template_full_path.split("/").last
+      file_elements = template_relative_path.split(".")
+      basename = file_elements.first
+      width, height = file_elements.second.split("x")
+      perspective = @perspectives.detect { |p| p.filename.to_s == basename }
+      markup = File.read(template_full_path)
+
+      perspective.add_template(width.to_i, height.to_i, markup)
     end
   end
 end
